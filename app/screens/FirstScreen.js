bad728fecff6e1f4561a590f30cb59c84916a6d4
Ryan Margono <rtm308@nyu.edu>
2017-08-03 16:18:45 -0400

    buttons

--- app/screens/FirstScreen.js
+++ app/screens/FirstScreen.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Text, View, Image, StatusBar } from 'react-native';
-import { Button, Grid, Col, Row } from 'react-native-elements';
+import { Button, Grid, Col, Row, Divider } from 'react-native-elements';
 import { Ionicons } from '@expo/vector-icons';
 import AppHeader from '../components/AppHeader';
 
@@ -11,31 +11,49 @@ export default class FirstScreen extends React.Component {
 	}
 	render() {
 		return (
-			<View style={
-				{
-					flex: 1,
-					justifyContent: 'center',
-					alignItems: 'center',
-				}
-			}>
+			<Grid>
 				<StatusBar hidden={true} />
-				<AppHeader 
-					button={
-						<Ionicons
-							name="ios-menu" size={20} color="black"
-							onPress={() => this.props.navigation.navigate('DrawerOpen')}
-						/>
-				}/> 
 
-				<Text style={{ fontSize: 30, color: 'green' }}>Screen 1</Text>
-				<Button 
-					raised 
-					iconRight
-					icon={{name:'add'}}
-					title='Add Drink'
-				/>
-				
-			</View>
+				<Row size={1}>
+					<AppHeader
+						button={
+							<Ionicons
+								name="ios-menu" size={20} color="black"
+								onPress={() => this.props.navigation.navigate('DrawerOpen')}
+							/>
+						} />
+				</Row>
+				<Divider style={{ backgroundColor: 'blue' }} />
+				<Row size={8}>
+					<Text style={{ fontSize: 30, color: 'green' }}>Screen 1</Text>
+				</Row>
+				<Divider style={{ backgroundColor: 'blue' }} />
+				<Row size={1}>
+					<Col size={1}>
+						<Button
+							raised
+							iconRight
+							backgroundColor='brown'
+							icon={{ name: 'add' }}
+							title='Add Drink'
+							buttonStyle={[{ height:"100%"}]}
+							containerViewStyle={{marginLeft:0, marginRight:0}}
+						/>
+					</Col>
+					
+					<Col size={1}>
+						<Button
+							raised
+							iconRight
+							backgroundColor='blue'
+							icon={{ name: 'add' }}
+							title='Add Water'
+							buttonStyle={[{ height:"100%"}]}
+							containerViewStyle={{marginLeft:0, marginRight:0}}
+						/>
+					</Col>
+				</Row>
+			</Grid>
 		)
 	}
 }
